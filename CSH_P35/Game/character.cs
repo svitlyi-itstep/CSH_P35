bf16e91953343b9c28be8332f18fd197d1b68706
svitlyi <svitlyi_a@itstep.academy>
2024-12-16 20:46:21 +0200

    Lesson Code

--- CSH_P35/Game/character.cs
+++ CSH_P35/Game/character.cs
@@ -10,11 +10,11 @@
 
     class Character
     {
-        string? name;
-        int health;
-        int damage;
-        int defence;
-        Race race;
+        protected string? name;
+        protected int health;
+        protected int damage;
+        protected int defence;
+        protected Race race;
 
         public string? Name
         {
@@ -67,13 +67,13 @@
             Console.WriteLine($" Раса: {race}");
         }
 
-        public int takeDamage(int damage)
+        public virtual int takeDamage(int damage)
         {
             health = Math.Max(health - damage, 0);
-            return health;
+            return damage;
         }
 
-        public int attack(Character target)
+        public virtual int attack(Character target)
         {
             int final_damage = (int)(this.damage * this.RaceAttackBonus(target.race));
             
@@ -100,10 +100,63 @@
                 return 1.1;
             }
         }
+
+        public override string ToString()
+        {
+            return $"Character(name={this.name}, health={this.health}, damage={this.damage}, " +
+                $"defence={this.defence}, race={this.race})";
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || !(obj is Character))
+                return false;
+            return this.name == ((Character)obj).name &&
+                this.health == ((Character)obj).health &&
+                this.damage == ((Character)obj).damage &&
+                this.defence == ((Character)obj).defence &&
+                this.race == ((Character)obj).race;
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
     }
 
     class Berserk: Character
     {
+        // Множник шкоди при низькому здоров`ї
+        double rageMultiplier = 1.5;
+        // Чи є останній шанс
+        bool lastChance = true;
 
+        public Berserk(string? name, int health, int damage, int defence, Race race = Race.Human) 
+            : base(name, health, damage, defence, race) { }
+        public Berserk() : this("Jonny", 100, 5, 0, Race.Human) { }
+
+        public override int takeDamage(int damage)
+        {
+            health = Math.Max(health - damage, 0);
+            // Механіка останнього шансу
+            if(this.health <= 0 && lastChance)
+            {
+                this.health = 1; lastChance = false;
+            }
+            return damage;
+        }
+
+        public override int attack(Character target)
+        {
+            int final_damage = (int)(this.damage * this.RaceAttackBonus(target.Race));
+            final_damage = this.health < 50 ? (int)(final_damage * rageMultiplier) : final_damage;
+            return target.takeDamage(final_damage);
+        }
+
+        public override string ToString()
+        {
+            return $"Berserk(name={this.name}, health={this.health}, damage={this.damage}, " +
+                $"defence={this.defence}, race={this.race}, rageMultiplier={this.rageMultiplier}, " +
+                $"lastChance={this.lastChance})";
+        }
     }
 }
--- CSH_P35/Game/main.cs
+++ CSH_P35/Game/main.cs
@@ -4,11 +4,20 @@ using CSH_P35.Game;
 
 class Program
 {
+    static void ShowAttackMessage(Character attacker, Character target, int damage)
+    {
+        Console.WriteLine($"{attacker.Name} атакував {target.Name} і наніс {damage} шкоди.");
+        Console.WriteLine($"У {target.Name} лишилося {target.Health} здоров`я.");
+    }
+    
     static void Main(string[] args)
     {
         Console.OutputEncoding = UTF8Encoding.UTF8;
         Console.InputEncoding = UTF8Encoding.UTF8;
+
         Berserk player1 = new Berserk();
+        Console.WriteLine(player1);
+        Console.WriteLine(player1.GetHashCode());
         player1.print();
         Console.WriteLine();
 
@@ -17,8 +26,12 @@ class Program
 
         while (player1.isAlive() && player2.isAlive())
         {
-            player1.attack(player2);
-            player2.attack(player1);
+            Console.WriteLine();
+            int pl1Damage = player1.attack(player2);
+            ShowAttackMessage(player1, player2, pl1Damage);
+            int pl2Damage = player2.attack(player1);
+            ShowAttackMessage(player2, player1, pl2Damage);
+
             Thread.Sleep(1000);
         }
 
@@ -29,3 +42,10 @@ class Program
 
     }
 }
+
+/*
+    Реалізувати механіку "Останнього шансу" для Берсерка: коли Берсерк отримує смертельну шкоду,
+    одноразово його здоров`я встановлюється як 1. Після цього наступна смертельна шкода має залишити
+    здоров`я на рівні 0.
+
+ */
