8eea0d73eed874ae5d340d0a1a1c39978fc2c49b
svitlyi <svitlyi_a@itstep.academy>
2025-01-15 19:39:10 +0200

    Lesson 9 code

---
+++ CSH_P35/ConsoleGame/game.cs
@@ -0,0 +1,68 @@
+﻿using System.Drawing;
+
+namespace CSH_P35.ConsoleGame.Game
+{
+    class Player
+    {
+        private Point pos;
+        public ConsoleColor color = ConsoleColor.White;
+        public Point Pos { 
+            get { return pos; } 
+            set { 
+                pos.X = Math.Max(value.X, 0);
+                pos.Y = Math.Max(value.Y, 0);
+            } 
+        }
+
+        public void Move(Point pos)
+        {
+            this.pos.X = Math.Max(this.pos.X + pos.X, 0);
+            this.pos.Y = Math.Max(this.pos.Y + pos.Y, 0);
+        }
+
+        public void Move(int x, int y)
+        {
+            this.Move(new Point(x, y));
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(pos.X, pos.Y);
+            Console.ForegroundColor = color;
+            Console.Write("██");
+            Console.ResetColor();
+        }
+
+        public int Distanse(Point point)
+        {
+            return (int)Math.Sqrt(
+                Math.Pow(this.pos.X - point.X, 2) + 
+                Math.Pow(this.pos.Y - point.Y, 2)
+            );
+        }
+    }
+
+    class Coin
+    {
+        private Point pos;
+        public ConsoleColor color = ConsoleColor.Yellow;
+        public Point Pos
+        {
+            get { return pos; }
+            set
+            {
+                pos.X = Math.Max(value.X, 0);
+                pos.Y = Math.Max(value.Y, 0);
+            }
+        }
+        public Coin(Point pos) { this.pos = pos; }
+        public Coin(int x, int y) : this(new Point(x, y)) { }
+        public void Draw() 
+        {
+            Console.SetCursorPosition(pos.X, pos.Y);
+            Console.ForegroundColor = this.color;
+            Console.Write("■");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ CSH_P35/ConsoleGame/main.cs
@@ -0,0 +1,43 @@
+﻿using CSH_P35.ConsoleGame.Game;
+using System.Drawing;
+
+namespace CSH_P35.ConsoleGame
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Player player = new Player();
+            player.Pos = new Point(3, 3);
+            Console.CursorVisible = false;
+
+            List<Coin> coinList = new List<Coin>();
+            coinList.Add(new Coin(13, 6));
+
+            while (true)
+            {
+                Console.Clear();
+                
+                foreach(var coin in coinList.ToList()) {
+                    if (player.Distanse(coin.Pos) < 1)
+                    {
+                        coinList.Remove(coin);
+                    }
+                    else
+                    {
+                        coin.Draw();
+                    }
+                }
+                player.Draw();
+                
+
+                ConsoleKey key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.D) { player.Move(1, 0); }
+                else if (key == ConsoleKey.A) { player.Move(-1, 0); }
+                else if (key == ConsoleKey.W) { player.Move(0, -1); }
+                else if (key == ConsoleKey.S) { player.Move(0, 1); }
+            }
+        }
+    }
+}
